e82fefb37caca1c13648483160ba4c752d1b93c6
fluffiesNFT <91221128+fluffiesNFT@users.noreply.github.com>
2021-09-29 10:21:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const http = require('http')
 
 const requestListener = function (req, res) {
-	console.log(req, res)
+	if(req.url.length < 2) {
+		let read = fs.createReadStream("index.html")
+		read.pipe(res)
+	} else {
+		let read = fs.createReadStream(__dirname + req.url)
+		read.pipe(res)
+	}
 }
 
 //initialize simple http server on port heroku supplies
